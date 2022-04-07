ce498103ecef4764cb69de3d0b4c782aa3584af0
mathieu-schertenleib <schertenleib.mathieu@gmail.com>
2022-04-07 10:44:21 +0200

    Proximity base

--- robotics_project/robotics_project/main.c
+++ robotics_project/robotics_project/main.c
@@ -12,185 +12,81 @@
 #include <audio/microphone.h>
 #include <sensors/VL53L0X/VL53L0X.h>
 #include <sensors/proximity.h>
-
-#include <audio_processing.h>
-#include <fft.h>
 #include <communications.h>
 #include <arm_math.h>
+#include <msgbus/messagebus.h>
 
-//uncomment to send the FFTs results from the real microphones
-//#define SEND_FROM_MIC
+messagebus_t bus;
+MUTEX_DECL(bus_lock);
+CONDVAR_DECL(bus_condvar);
 
-//uncomment to use double buffering to send the FFT to the computer
-#define DOUBLE_BUFFERING
-
-static void serial_start(void)
-{
-	static SerialConfig ser_cfg = {
-	    115200,
-	    0,
-	    0,
-	    0,
-	};
+static void serial_start(void) {
+	static SerialConfig ser_cfg = { 115200, 0, 0, 0, };
 
 	sdStart(&SD3, &ser_cfg); // UART3.
 }
 
-static void timer12_start(void){
-    //General Purpose Timer configuration   
-    //timer 12 is a 16 bit timer so we can measure time
-    //to about 65ms with a 1Mhz counter
-    static const GPTConfig gpt12cfg = {
-        1000000,        /* 1MHz timer clock in order to measure uS.*/
-        NULL,           /* Timer callback.*/
-        0,
-        0
-    };
+static void timer12_start(void) {
+	//General Purpose Timer configuration
+	//timer 12 is a 16 bit timer so we can measure time
+	//to about 65ms with a 1Mhz counter
+	static const GPTConfig gpt12cfg = { 1000000, /* 1MHz timer clock in order to measure uS.*/
+	NULL, /* Timer callback.*/
+	0, 0 };
 
-    gptStart(&GPTD12, &gpt12cfg);
-    //let the timer count to max value
-    gptStartContinuous(&GPTD12, 0xFFFF);
+	gptStart(&GPTD12, &gpt12cfg);
+	//let the timer count to max value
+	gptStartContinuous(&GPTD12, 0xFFFF);
 }
 
-int main(void)
-{
+int main(void) {
 
-    halInit();
-    chSysInit();
-    mpu_init();
+	halInit();
+	chSysInit();
+	mpu_init();
 
-    //starts the serial communication
-    serial_start();
-    //starts the USB communication
-    usb_start();
-    //starts timer 12
-    timer12_start();
-    //inits the motors
-    motors_init();
-    //inits the proximity sensors
-    //proximity_start();
-    // calibrate the proximity sensors
-    //calibrate_ir();
+	messagebus_init(&bus, &bus_lock, &bus_condvar);
 
-#if 0
-    //temp tab used to store values in complex_float format
-    //needed bx doFFT_c
-    static complex_float temp_tab[FFT_SIZE];
-    //send_tab is used to save the state of the buffer to send (double buffering)
-    //to avoid modifications of the buffer while sending it
-    static float send_tab[FFT_SIZE];
+	//starts the serial communication
+	//serial_start();
+	//starts the USB communication
+	usb_start();
+	//starts timer 12
+	timer12_start();
+	//inits the motors
+	//motors_init();
+	//inits the proximity sensors
+	proximity_start();
+	// calibrate the proximity sensors
+	//calibrate_ir();
 
-#ifdef SEND_FROM_MIC
-    //starts the microphones processing thread.
-    //it calls the callback given in parameter when samples are ready
-    mic_start(&processAudioData);
-#endif  /* SEND_FROM_MIC */
+	int prox_values[8] = { 0 };
 
-    /* Infinite loop. */
-    while (1) {
-#ifdef SEND_FROM_MIC
-        //waits until a result must be sent to the computer
-        wait_send_to_computer();
-#ifdef DOUBLE_BUFFERING
-        //we copy the buffer to avoid conflicts
-        arm_copy_f32(get_audio_buffer_ptr(LEFT_OUTPUT), send_tab, FFT_SIZE);
-        SendFloatToComputer((BaseSequentialStream *) &SD3, send_tab, FFT_SIZE);
-#else
-        SendFloatToComputer((BaseSequentialStream *) &SD3, get_audio_buffer_ptr(LEFT_OUTPUT), FFT_SIZE);
-#endif  /* DOUBLE_BUFFERING */
-#else
-        //time measurement variables
-        volatile uint16_t time_fft = 0;
-        volatile uint16_t time_mag  = 0;
+	/* Infinite loop. */
+	while (1) {
 
-        float* bufferCmplxInput = get_audio_buffer_ptr(LEFT_CMPLX_INPUT);
-        float* bufferOutput = get_audio_buffer_ptr(LEFT_OUTPUT);
+		for (uint8_t prox_sensor_number = 0; prox_sensor_number < 8;
+				++prox_sensor_number) {
+			prox_values[prox_sensor_number] = get_calibrated_prox(
+					prox_sensor_number);
+		}
+		chprintf((BaseSequentialStream *) &SDU1,
+				"Prox. values: {%d, %d, %d, %d, %d, %d, %d, %d}\r\n",
+				prox_values[0], prox_values[1], prox_values[2], prox_values[3],
+				prox_values[4], prox_values[5], prox_values[6], prox_values[7]);
 
-        uint16_t size = ReceiveInt16FromComputer((BaseSequentialStream *) &SD3, bufferCmplxInput, FFT_SIZE);
+		/*prox_values[0] = get_calibrated_prox(0);
+		 chprintf((BaseSequentialStream *) &SDU1, "Prox. value 0: %d",
+		 prox_values[0]);*/
 
-        if(size == FFT_SIZE){
-            /*
-            *   Optimized FFT
-            */
-            
-            chSysLock();
-            //reset the timer counter
-            GPTD12.tim->CNT = 0;
-
-            doFFT_optimized(FFT_SIZE, bufferCmplxInput);
-
-            time_fft = GPTD12.tim->CNT;
-            chSysUnlock();
-
-            /*
-            *   End of optimized FFT
-            */
-
-            /*
-            *   Non optimized FFT
-            */
-
-            // //need to convert the float buffer into complex_float struct array
-            // for(uint16_t i = 0 ; i < (2*FFT_SIZE) ; i+=2){
-            //     temp_tab[i/2].real = bufferCmplxInput[i];
-            //     temp_tab[i/2].imag = bufferCmplxInput[i+1];
-            // }
-
-            // chSysLock();
-            // //reset the timer counter
-            // GPTD12.tim->CNT = 0;
-
-            // //do a non optimized FFT
-            // doFFT_c(FFT_SIZE, temp_tab);
-
-            // time_fft = GPTD12.tim->CNT;
-            // chSysUnlock();
-            
-            // //reconverts the result into a float buffer
-            // for(uint16_t i = 0 ; i < (2*FFT_SIZE) ; i+=2){
-            //     bufferCmplxInput[i] = temp_tab[i/2].real;
-            //     bufferCmplxInput[i+1] = temp_tab[i/2].imag;
-            // }
-
-            /*
-            *   End of non optimized FFT
-            */
-
-            chSysLock();
-            //reset the timer counter
-            GPTD12.tim->CNT = 0;
-
-            arm_cmplx_mag_f32(bufferCmplxInput, bufferOutput, FFT_SIZE);
-
-            time_mag = GPTD12.tim->CNT;
-            chSysUnlock();
-
-            SendFloatToComputer((BaseSequentialStream *) &SD3, bufferOutput, FFT_SIZE);
-            //chprintf((BaseSequentialStream *) &SDU1, "time fft = %d us, time magnitude = %d us\n",time_fft, time_mag);
-
-        }
-#endif  /* SEND_FROM_MIC */
-    }
-#endif
-
-    int prox_values[8] = {0};
-
-    /* Infinite loop. */
-    while (1) {
-
-    	/*for (uint8_t prox_sensor_number = 0; prox_sensor_number < 8; ++prox_sensor_number) {
-    		prox_values[prox_sensor_number] = get_calibrated_prox(prox_sensor_number);
-    	}
-
-    	chprintf((BaseSequentialStream *) &SDU1, "Prox value 0 = %d", prox_values[0]);*/
-    	chThdSleepMilliseconds(100);
-    }
+		//chprintf((BaseSequentialStream *) &SDU1, "Ping");
+		chThdSleepMilliseconds(200);
+	}
 }
 
 #define STACK_CHK_GUARD 0xe2dee396
 uintptr_t __stack_chk_guard = STACK_CHK_GUARD;
 
-void __stack_chk_fail(void)
-{
-    chSysHalt("Stack smashing detected");
+void __stack_chk_fail(void) {
+	chSysHalt("Stack smashing detected");
 }
