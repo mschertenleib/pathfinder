1eac8957aad7c6435a89c39d8541d6b9ca8a978d
mathieu-schertenleib <schertenleib.mathieu@gmail.com>
2022-04-08 21:17:55 +0200

    Working TOF and proximity

--- robotics_project/robotics_project/main.c
+++ robotics_project/robotics_project/main.c
@@ -12,9 +12,10 @@
 #include <audio/microphone.h>
 #include <sensors/VL53L0X/VL53L0X.h>
 #include <sensors/proximity.h>
-#include <communications.h>
 #include <arm_math.h>
 #include <msgbus/messagebus.h>
+
+#include "communications.h"
 
 messagebus_t bus;
 MUTEX_DECL(bus_lock);
@@ -30,9 +31,9 @@ static void timer12_start(void) {
 	//General Purpose Timer configuration
 	//timer 12 is a 16 bit timer so we can measure time
 	//to about 65ms with a 1Mhz counter
-	static const GPTConfig gpt12cfg = { 1000000, /* 1MHz timer clock in order to measure uS.*/
-	NULL, /* Timer callback.*/
-	0, 0 };
+	static const GPTConfig gpt12cfg = { .frequency = 1000000, /* 1MHz timer clock in order to measure uS.*/
+	.callback = NULL, /* Timer callback.*/
+	.cr2 = 0, .dier = 0 };
 
 	gptStart(&GPTD12, &gpt12cfg);
 	//let the timer count to max value
@@ -45,25 +46,32 @@ int main(void) {
 	chSysInit();
 	mpu_init();
 
+	//inits the messagebus
 	messagebus_init(&bus, &bus_lock, &bus_condvar);
 
 	//starts the serial communication
-	//serial_start();
+	serial_start();
 	//starts the USB communication
 	usb_start();
 	//starts timer 12
 	timer12_start();
 	//inits the motors
-	//motors_init();
+	motors_init();
+
 	//inits the proximity sensors
 	proximity_start();
-	// calibrate the proximity sensors
-	//calibrate_ir();
+	//calibrates the proximity sensors
+	calibrate_ir();
 
-	int prox_values[8] = { 0 };
+	//inits the TOF sensor
+	VL53L0X_start();
 
 	/* Infinite loop. */
 	while (1) {
+
+#define SEND_PROX_VALUES
+#ifdef SEND_PROX_VALUES
+		int prox_values[8] = {0};
 
 		for (uint8_t prox_sensor_number = 0; prox_sensor_number < 8;
 				++prox_sensor_number) {
@@ -74,13 +82,16 @@ int main(void) {
 				"Prox. values: {%d, %d, %d, %d, %d, %d, %d, %d}\r\n",
 				prox_values[0], prox_values[1], prox_values[2], prox_values[3],
 				prox_values[4], prox_values[5], prox_values[6], prox_values[7]);
+#endif
 
-		/*prox_values[0] = get_calibrated_prox(0);
-		 chprintf((BaseSequentialStream *) &SDU1, "Prox. value 0: %d",
-		 prox_values[0]);*/
+#define SEND_TOF_VALUE
+#ifdef SEND_TOF_VALUE
+		uint16_t dist_mm = VL53L0X_get_dist_mm();
+		chprintf((BaseSequentialStream *) &SDU1, "TOF dist. [mm]: %d\r\n",
+				dist_mm);
+#endif
 
-		//chprintf((BaseSequentialStream *) &SDU1, "Ping");
-		chThdSleepMilliseconds(200);
+		chThdSleepMilliseconds(100);
 	}
 }
 
