50f9f2c211f0ca57fe2ac834a92ce57cb7e4b6cd
mathieu-schertenleib <schertenleib.mathieu@gmail.com>
2022-04-30 18:41:04 +0200

    Parameterize physical map dimension and resolution

--- robotics_project/slam/environment_map.py
+++ robotics_project/slam/environment_map.py
@@ -1,25 +1,68 @@
 import numpy as np
+import cv2
 
 
 class Environment_map:
 
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.walls = np.zeros((height, width), dtype=np.bool8)
+    def __init__(self, width_mm, height_mm, cell_size_mm):
+        self.cell_size_mm = cell_size_mm
+        self.width_mm = width_mm
+        self.height_mm = height_mm
+        self.width = self.__mm_to_index(self.width_mm)
+        self.height = self.__mm_to_index(self.height_mm)
+        self.walls = np.zeros((self.height, self.width), dtype=np.bool8)
 
-    def get_value_at(self, x, y):
-        return self.walls[y][x]
+    def __mm_to_index(self, mm):
+        return int(mm / self.cell_size_mm)
 
-    def set_value_at(self, x, y, value):
-        self.walls[y][x] = value
+    def get_at_index(self, x_index, y_index):
+        return self.walls[y_index][x_index]
 
-    # coordinates in range [0, 1[
+    def set_at_index(self, x_index, y_index, value):
+        self.walls[y_index][x_index] = value
+
+    def get_at_mm(self, x_mm, y_mm):
+        x_index = self.__mm_to_index(x_mm)
+        y_index = self.__mm_to_index(y_mm)
+        return self.get_at_index(x_index=x_index, y_index=y_index)
+
+    def set_at_mm(self, x_mm, y_mm, value):
+        x_index = self.__mm_to_index(x_mm)
+        y_index = self.__mm_to_index(y_mm)
+        self.set_at_index(x_index=x_index, y_index=y_index, value=value)
+
+    # normalized coordinates in range [0, 1[
     def generate_horizontal_wall(self, x1, x2, y):
-        for x in range(int(x1 * self.width), int(x2 * self.width)):
-            self.set_value_at(x, int(y * self.height), True)
+        y_index = int(y * self.height)
+        for x_index in range(int(x1 * self.width), int(x2 * self.width)):
+            self.set_at_index(x_index, y_index, True)
 
-    # coordinates in range [0, 1[
+    # normalized coordinates in range [0, 1[
     def generate_vertical_wall(self, x, y1, y2):
-        for y in range(int(y1 * self.height), int(y2 * self.height)):
-            self.set_value_at(int(x * self.width), y, True)
+        x_index = int(x * self.width)
+        for y_index in range(int(y1 * self.height), int(y2 * self.height)):
+            self.set_at_index(x_index, y_index, True)
+
+    def raycast(self, start_x_mm, start_y_mm, angle_rad):
+        x_mm, y_mm = start_x_mm, start_y_mm
+        delta_mm = self.cell_size_mm / 10
+        while x_mm > 0 and x_mm < self.width_mm and y_mm > 0 and y_mm < self.height_mm:
+            if self.get_at_mm(x_mm, y_mm):
+                return np.sqrt((x_mm - start_x_mm)**2 + (y_mm - start_y_mm)**2)
+            x_mm += delta_mm * np.cos(angle_rad)
+            y_mm += delta_mm * np.sin(angle_rad)
+
+        return np.Infinity
+
+    def to_image(self, img, color):
+        img_height_px = img.shape[0]
+        #img_width_px = img.shape[1]
+        cell_size_px = img_height_px / self.height
+        for y_index in range(self.height):
+            for x_index in range(self.width):
+                if self.get_at_index(x_index, y_index):
+                    p1_px = (int(x_index * cell_size_px),
+                             int(y_index * cell_size_px))
+                    p2_px = (int((x_index + 1) * cell_size_px),
+                             int((y_index + 1) * cell_size_px))
+                    cv2.rectangle(img, p1_px, p2_px, color, -1)
--- robotics_project/slam/open_loop.py
+++ robotics_project/slam/open_loop.py
@@ -1,113 +1,93 @@
-import matplotlib.pyplot as plt
 import numpy as np
-import cv2 as cv
+import cv2
 import environment_map
 
 
-def create_lidar_data():
-    arr = np.zeros((360, 2))
-    for i in range(arr.shape[0]):
-        arr[i] = (i, 30 + np.random.normal() * 5)
-    return arr
-
-
-def raycast(walls: environment_map.Environment_map, start_x, start_y, angle):
-    px, py = start_x, start_y
-    delta = 0.1
-    while int(py) > 0 and int(py) < walls.height and int(px) > 0 and int(px) < walls.width:
-        if walls.get_value_at(x=int(px), y=int(py)):
-            return np.sqrt((px - start_x)**2 + (py - start_y)**2)
-        px += delta * np.cos(angle)
-        py += delta * np.sin(angle)
-
-    return np.Infinity
-
-
-def get_lidar_data(walls, robot_x, robot_y, tof_sensor_offset):
+def get_robot_data(true_map: environment_map.Environment_map, tof_sensor_offset_mm):
     num_samples = 100
-    arr = np.zeros((num_samples, 2))
+    arr = np.zeros((num_samples, 4))
+    robot_x_mm = true_map.width_mm / 2
+    robot_y_mm = true_map.height_mm / 2
     for i in range(num_samples):
-        angle_deg = i / num_samples * 360
-        angle_rad = np.radians(angle_deg)
-        start_x = robot_x + tof_sensor_offset * np.cos(angle_rad)
-        start_y = robot_y + tof_sensor_offset * np.sin(angle_rad)
-        exact_distance = raycast(walls, start_x, start_y, angle_rad)
-        distance = exact_distance * (1 + (np.random.rand() * 2 - 1) * 0.1)
-        arr[i] = (angle_deg, distance)
+        angle_rad = i / num_samples * 2 * np.pi
+        start_x_mm = robot_x_mm + tof_sensor_offset_mm * np.cos(angle_rad)
+        start_y_mm = robot_y_mm + tof_sensor_offset_mm * np.sin(angle_rad)
+        exact_distance_mm = true_map.raycast(start_x_mm, start_y_mm, angle_rad)
+        distance_mm = exact_distance_mm * \
+            (1 + (np.random.rand() * 2 - 1) * 0.1)
+        arr[i] = (robot_x_mm, robot_y_mm, angle_rad, distance_mm)
     return arr
 
 
-def draw_robot(img, robot_x, robot_y, robot_angle, robot_radius):
+def draw_robot(img, robot_x_mm, robot_y_mm, robot_angle_rad, robot_radius_mm):
     color = (0, 0, 255)
 
-    cv.circle(img, (int(robot_x), int(robot_y)), robot_radius, color)
+    cv2.circle(img, (int(robot_x_mm), int(robot_y_mm)),
+               robot_radius_mm, color, 2)
 
-    cv.line(img, (int(robot_x), int(robot_y)), (int(robot_x + 1.5 * robot_radius * np.cos(np.radians(robot_angle))),
-            int(robot_y + 1.5 * robot_radius * np.sin(np.radians(robot_angle)))), color)
+    cv2.line(img, (int(robot_x_mm), int(robot_y_mm)), (int(robot_x_mm + robot_radius_mm *
+             np.cos(robot_angle_rad)), int(robot_y_mm + robot_radius_mm * np.sin(robot_angle_rad))), color, 2)
 
 
 if __name__ == '__main__':
 
-    width = 2*160
-    height = 2*120
-    robot_x = width / 2
-    robot_y = height / 2
-    robot_radius = 2*5
-    robot_delta = 2*robot_radius
-    tof_sensor_offset = robot_radius
+    width_mm = 640
+    height_mm = 480
+    cell_size_mm = 5
+    robot_radius_mm = 50
+    tof_sensor_offset_mm = robot_radius_mm
+    robot_delta_mm = robot_radius_mm
+    width_px = width_mm
+    height_px = height_mm
 
-    walls = environment_map.Environment_map(width, height)
-    walls.generate_horizontal_wall(x1=0.1, x2=0.9, y=0.2)
-    walls.generate_horizontal_wall(x1=0.5, x2=0.8, y=0.7)
-    walls.generate_vertical_wall(x=0.1, y1=0.1, y2=0.9)
-    walls.generate_vertical_wall(x=0.7, y1=0.3, y2=0.7)
-    walls.generate_vertical_wall(x=0.4, y1=0.6, y2=0.9)
+    true_map = environment_map.Environment_map(
+        width_mm=width_mm, height_mm=height_mm, cell_size_mm=1)
+    true_map.generate_horizontal_wall(x1=0.1, x2=0.9, y=0.2)
+    true_map.generate_horizontal_wall(x1=0.5, x2=0.8, y=0.7)
+    true_map.generate_vertical_wall(x=0.1, y1=0.1, y2=0.9)
+    true_map.generate_vertical_wall(x=0.7, y1=0.3, y2=0.7)
+    true_map.generate_vertical_wall(x=0.4, y1=0.6, y2=0.9)
 
     constructed_map = environment_map.Environment_map(
-        width=width, height=height)
+        width_mm=width_mm, height_mm=height_mm, cell_size_mm=cell_size_mm)
 
-    cv.namedWindow('image', cv.WINDOW_NORMAL)
+    cv2.namedWindow('image')  # , cv2.WINDOW_NORMAL)
 
-    while True:
+    robot_data = get_robot_data(true_map, tof_sensor_offset_mm)
 
-        lidar_data = get_lidar_data(
-            walls, robot_x, robot_y, tof_sensor_offset)
+    for robot_x_mm, robot_y_mm, robot_angle_rad, distance_mm in robot_data:
 
-        img = np.ones((height, width, 3), np.uint8) * 255
+        img = np.ones((height_px, width_px, 3), np.uint8) * 255
 
-        for angle, distance in lidar_data:
-            if distance < np.Infinity:
-                wall_x = robot_x + (distance + tof_sensor_offset) * \
-                    np.cos(np.radians(angle))
-                wall_y = robot_y + (distance + tof_sensor_offset) * \
-                    np.sin(np.radians(angle))
-                if int(wall_y) < height and int(wall_x) < width:
-                    constructed_map.set_value_at(
-                        x=int(wall_x), y=int(wall_y), value=True)
-                    cv.line(img, (int(robot_x + tof_sensor_offset * np.cos(np.radians(angle))), int(robot_y +
-                            tof_sensor_offset * np.sin(np.radians(angle)))), (int(wall_x), int(wall_y)), (200, 200, 200))
+        if distance_mm < np.Infinity:
+            wall_x_mm = robot_x_mm + (distance_mm + tof_sensor_offset_mm) * \
+                np.cos(robot_angle_rad)
+            wall_y_mm = robot_y_mm + (distance_mm + tof_sensor_offset_mm) * \
+                np.sin(robot_angle_rad)
+            constructed_map.set_at_mm(wall_x_mm, wall_y_mm, True)
+            cv2.line(img, (int(robot_x_mm), int(robot_y_mm)),
+                     (int(wall_x_mm), int(wall_y_mm)), (200, 200, 200), cell_size_mm)
 
-        for y in range(height):
-            for x in range(width):
-                if constructed_map.get_value_at(x, y):
-                    cv.circle(img, (x, y), 0, (0, 0, 0))
-                elif walls.get_value_at(x, y):
-                    cv.circle(img, (x, y), 0, (150, 150, 150))
+        draw_robot(img, robot_x_mm, robot_y_mm,
+                   robot_angle_rad, robot_radius_mm)
 
-        draw_robot(img=img, robot_x=robot_x,
-                   robot_y=robot_y, robot_angle=30, robot_radius=robot_radius)
+        true_map.to_image(img, (255, 0, 0))
+        constructed_map.to_image(img, (0, 0, 0))
 
-        cv.imshow('image', img)
-        k = cv.waitKey(0)
-        if k == ord('w'):
-            robot_y -= robot_delta
-        if k == ord('a'):
-            robot_x -= robot_delta
-        if k == ord('s'):
-            robot_y += robot_delta
-        if k == ord('d'):
-            robot_x += robot_delta
-        if k == 27:  # Esc
-            break
+        cv2.imshow('image', img)
+        cv2.waitKey(1)
 
-    cv.destroyAllWindows()
+        #k = cv2.waitKey(0)
+        # if k == ord('w'):
+        #    robot_y_mm -= robot_delta_mm
+        # if k == ord('a'):
+        #    robot_x_mm -= robot_delta_mm
+        # if k == ord('s'):
+        #    robot_y_mm += robot_delta_mm
+        # if k == ord('d'):
+        #    robot_x_mm += robot_delta_mm
+        #if k == 27:  # Esc
+        #    break
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
