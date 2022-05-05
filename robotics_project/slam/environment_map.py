b80f3a42d330bf0a0444462d29277bf8718a4807
mathieu-schertenleib <schertenleib.mathieu@gmail.com>
2022-05-05 07:39:52 +0200

    Merge Grid_map and Constructed_map implementations

--- robotics_project/slam/environment_map.py
+++ robotics_project/slam/environment_map.py
@@ -5,51 +5,74 @@ from pathfinding.core.grid import Grid
 from pathfinding.finder.a_star import AStarFinder
 
 
-class Grid_map:
+class Environment_map:
 
-    def __init__(self, width_mm, height_mm, width_px, height_px):
-        self.cell_size_mm = width_mm / width_px
+    OCCUPIED_THRESHOLD = 0.25
+    FREE_THRESHOLD = 0.75
+
+    def __init__(self, width_mm, height_mm, cell_size_mm):
+        self.cell_size_mm = cell_size_mm
         self.width_mm = width_mm
         self.height_mm = height_mm
-        self.width = width_px
-        self.height = height_px
-        self.cells = np.ones((self.height, self.width))
+        self.width = self.__val_mm_to_index(self.width_mm)  # width in cells
+        self.height = self.__val_mm_to_index(self.height_mm)  # height in cells
+        self.free_samples = np.zeros((self.height, self.width), dtype=int)
+        self.total_samples = np.zeros((self.height, self.width), dtype=int)
+        self.cells = np.ones((self.height, self.width)) * 0.5
 
-    def line(self, pt1_mm, pt2_mm, thickness=1):
+    def set_all_free(self):
+        self.cells = np.ones((self.height, self.width))
+    
+    def occupied_line(self, pt1_mm, pt2_mm, thickness=1):
         pt1_index = self.__point_mm_to_index(pt1_mm)
         pt2_index = self.__point_mm_to_index(pt2_mm)
         cv2.line(self.walls, pt1_index, pt2_index, 0, thickness)
     
-    def line_normalized(self, pt1, pt2, thickness=1):
+    def occupied_line_normalized(self, pt1, pt2, thickness=1):
         pt1_index = (int(pt1[0] * self.width_mm), int(pt1[1] * self.height_mm))
         pt2_index = (int(pt2[0] * self.width_mm), int(pt2[1] * self.height_mm))
         cv2.line(self.cells, pt1_index, pt2_index, 0, thickness)
     
-    def rectangle(self, pt1_mm, pt2_mm, thickness=1):
+    def occupied_rectangle(self, pt1_mm, pt2_mm, thickness=1):
         pt1_index = self.__point_mm_to_index(pt1_mm)
         pt2_index = self.__point_mm_to_index(pt2_mm)
         cv2.rectangle(self.walls, pt1_index, pt2_index, 0, thickness)
     
-    def rectangle_normalized(self, pt1, pt2, thickness=1):
+    def occupied_rectangle_normalized(self, pt1, pt2, thickness=1):
         pt1_index = (int(pt1[0] * self.width_mm), int(pt1[1] * self.height_mm))
         pt2_index = (int(pt2[0] * self.width_mm), int(pt2[1] * self.height_mm))
         cv2.rectangle(self.cells, pt1_index, pt2_index, 0, thickness)
 
-    def circle(self, center_mm, radius_mm, thickness=1):
+    def occupied_circle(self, center_mm, radius_mm, thickness=1):
         center = self.__point_mm_to_index(center_mm)
         radius = self.__val_mm_to_index(radius_mm)
         cv2.circle(self.cells, center, radius, 0, thickness)
 
-    def to_image(self):
-        return self.cells
+    def free_line(self, pt1_mm, pt2_mm, thickness=1):
+        pt1_index = self.__point_mm_to_index(pt1_mm)
+        pt2_index = self.__point_mm_to_index(pt2_mm)
+        cv2.line(self.walls, pt1_index, pt2_index, 1, thickness)
+    
+    def free_line_normalized(self, pt1, pt2, thickness=1):
+        pt1_index = (int(pt1[0] * self.width_mm), int(pt1[1] * self.height_mm))
+        pt2_index = (int(pt2[0] * self.width_mm), int(pt2[1] * self.height_mm))
+        cv2.line(self.cells, pt1_index, pt2_index, 1, thickness)
+    
+    def free_rectangle(self, pt1_mm, pt2_mm, thickness=1):
+        pt1_index = self.__point_mm_to_index(pt1_mm)
+        pt2_index = self.__point_mm_to_index(pt2_mm)
+        cv2.rectangle(self.walls, pt1_index, pt2_index, 1, thickness)
+    
+    def free_rectangle_normalized(self, pt1, pt2, thickness=1):
+        pt1_index = (int(pt1[0] * self.width_mm), int(pt1[1] * self.height_mm))
+        pt2_index = (int(pt2[0] * self.width_mm), int(pt2[1] * self.height_mm))
+        cv2.rectangle(self.cells, pt1_index, pt2_index, 1, thickness)
 
-    def walkable(self, radius_mm):
-        kernel = disk_kernel(self.__val_mm_to_index(radius_mm))
-        # This method requires a free cell to be 0
-        inverted_cells = 1 - self.cells
-        filtered = cv2.filter2D(src=inverted_cells, ddepth=-1, kernel=kernel)
-        return 1 - filtered
-
+    def free_circle(self, center_mm, radius_mm, thickness=1):
+        center = self.__point_mm_to_index(center_mm)
+        radius = self.__val_mm_to_index(radius_mm)
+        cv2.circle(self.cells, center, radius, 1, thickness)
+    
     def raycast(self, start_pos_mm, angle_rad, default_distance_mm):
         """
         @brief                      Returns the distance from start_pos_mm to the closest obstacle in the angle_rad direction, or default_distance_mm if no obstacle intersection was found
@@ -72,42 +95,13 @@ class Grid_map:
 
         return default_distance_mm
 
-    def __x_mm_is_in_grid(self, x_mm):
-        return x_mm > 0 and x_mm < self.width_mm
 
-    def __y_mm_is_in_grid(self, y_mm):
-        return y_mm > 0 and y_mm < self.height_mm
-
-    def __point_mm_is_in_grid(self, point_mm):
-        x_mm, y_mm = point_mm
-        return self.__x_mm_is_in_grid(x_mm) and self.__y_mm_is_in_grid(y_mm)
-
-    def __val_mm_to_index(self, val):
-        return int(val / self.cell_size_mm)
-
-    def __point_mm_to_index(self, point):
-        return self.__val_mm_to_index(point[0]), self.__val_mm_to_index(point[1])
-
-
-class Constructed_map:
-
-    OCCUPIED_THRESHOLD = 0.25
-    FREE_THRESHOLD = 0.75
-
-    def __init__(self, width_mm, height_mm, cell_size_mm):
-        self.cell_size_mm = cell_size_mm
-        self.width_mm = width_mm
-        self.height_mm = height_mm
-        self.width = self.__val_mm_to_index(self.width_mm)  # width in cells
-        self.height = self.__val_mm_to_index(self.height_mm)  # height in cells
-        self.free_samples = np.zeros((self.height, self.width), dtype=int)
-        self.total_samples = np.zeros((self.height, self.width), dtype=int)
-        self.cells = np.ones((self.height, self.width)) * 0.5
-
+    #FIXME: bug where values go from almost 0 to almost 1 instantly (or the oppposite)
+    
     def construct(self, robot_pos_mm, robot_angle_rad, robot_radius_mm, tof_distance_mm, tof_sensor_offset_mm, tof_max_distance_mm):
 
         # Free disk where the robot is located
-        self.__set_free_disk(center_mm=robot_pos_mm, radius_mm=robot_radius_mm)
+        self.free_circle(center_mm=robot_pos_mm, radius_mm=robot_radius_mm, thickness=-1)
 
         cos_angle = np.cos(robot_angle_rad)
         sin_angle = np.sin(robot_angle_rad)
@@ -134,8 +128,9 @@ class Constructed_map:
             # Free line of sight
             self.__sample_free_line((tof_x_mm, tof_y_mm), (line_end_x_mm, line_end_y_mm))
 
-    def to_image(self, height, width):
         self.__update_cells()
+
+    def to_image(self, height, width):
         return cv2.resize(src=self.cells, dsize=(width, height), interpolation=cv2.INTER_NEAREST)
 
     def walkable(self, radius_mm):
@@ -143,6 +138,7 @@ class Constructed_map:
         # An obstacle is a positive value, a free cell is a 0
         obstacles = np.ones((self.height, self.width))
         obstacles[self.cells > self.FREE_THRESHOLD] = 0
+        cv2.rectangle(obstacles, (0, 0), (self.width-1, self.height-1), 1)
         filtered = cv2.filter2D(src=obstacles, ddepth=-1, kernel=kernel)
         walkable_grid = 1 - np.clip(filtered, 0, 1)
         return walkable_grid
@@ -154,20 +150,9 @@ class Constructed_map:
     def find_path(self, start_mm, goal_mm, robot_radius_mm):
         start = self.__point_mm_to_index(start_mm)
         goal = self.__point_mm_to_index(goal_mm)
-        path, runs = self.__find_path(start, goal, robot_radius_mm)
-
-        print('Operations:', runs)
-        if len(path) > 0:
-            print('Path length:', len(path))
-        else:
-            print('No path from start to goal found')
-        
-        img = np.zeros((self.height, self.width))
-        for x, y in path:
-            img[y][x] = 1
-        img = cv2.resize(src=img, dsize=(640, 480),
-                     interpolation=cv2.INTER_NEAREST)
-        cv2.imshow('map', img)
+        path = self.__find_path(start, goal, robot_radius_mm)
+        path = [self.__index_to_point_mm(x, y) for x, y in path]
+        return path
 
     def __x_mm_is_in_grid(self, x_mm):
         return x_mm > 0 and x_mm < self.width_mm
@@ -179,11 +164,17 @@ class Constructed_map:
         x_mm, y_mm = point_mm
         return self.__x_mm_is_in_grid(x_mm) and self.__y_mm_is_in_grid(y_mm)
 
-    def __val_mm_to_index(self, val):
-        return int(val / self.cell_size_mm)
+    def __val_mm_to_index(self, val_mm):
+        return int(val_mm / self.cell_size_mm)
 
-    def __point_mm_to_index(self, point):
-        return self.__val_mm_to_index(point[0]), self.__val_mm_to_index(point[1])
+    def __point_mm_to_index(self, point_mm):
+        return self.__val_mm_to_index(point_mm[0]), self.__val_mm_to_index(point_mm[1])
+    
+    def __index_to_val_mm(self, index):
+        return (index + 0.5) * self.cell_size_mm
+
+    def __index_to_point_mm(self, index_x, index_y):
+        return self.__index_to_val_mm(index_x), self.__index_to_val_mm(index_y)
 
     def __sample_free_line(self, pt1_mm, pt2_mm):
         pt1 = self.__point_mm_to_index(pt1_mm)
@@ -195,22 +186,14 @@ class Constructed_map:
         cv2.add(self.free_samples, line_to_add, self.free_samples)
         cv2.add(self.total_samples, line_to_add, self.total_samples)
 
-    def __set_free_disk(self, center_mm, radius_mm):
-        center = self.__point_mm_to_index(center_mm)
-        radius = self.__val_mm_to_index(radius_mm)
-        cv2.circle(img=self.cells, center=center, radius=radius, color=1, thickness=-1)
-
-    # FIXME: some cells sometimes directly go from 1 to 0 -> why?
-
     def __update_cells(self):
         # Required for cv2.divide to be defined when dividing by 0
         assert self.total_samples.dtype == int
 
         means = cv2.divide(self.free_samples, self.total_samples)
-        means[self.total_samples == 0] = 0.5
         free_probabilities = 0.5 + (means - 0.5) * (1 - np.power(0.5, self.total_samples))
-        uncertain_cells = np.logical_and(self.cells > 0, self.cells < 1)
-        self.cells = np.where(uncertain_cells, free_probabilities, self.cells)
+        cells_to_update = np.logical_and(self.cells > 0, self.cells < 1, self.total_samples > 0)
+        self.cells = np.where(cells_to_update, free_probabilities, self.cells)
     
     def __find_path(self, start_coords, goal_coords, robot_radius_mm):
         walkable_cells = self.walkable(robot_radius_mm)
@@ -219,7 +202,7 @@ class Constructed_map:
         end = grid.node(goal_coords[0], goal_coords[1])
         finder = AStarFinder(diagonal_movement=DiagonalMovement.always)
         path, runs = finder.find_path(start, end, grid)
-        return path, runs
+        return path
 
 def disk_kernel(radius):
     """
--- robotics_project/slam/open_loop.py
+++ robotics_project/slam/open_loop.py
@@ -3,18 +3,21 @@ import cv2
 import environment_map as envmap
 
 
-def robot_data_generator(true_map: envmap.Grid_map, tof_sensor_offset_mm, tof_default_distance_mm, add_uncertainty = False):
+def robot_data_generator(true_map: envmap.Environment_map, tof_sensor_offset_mm, tof_default_distance_mm, add_uncertainty=False):
     i = 0
     while(True):
 
-        # Robot rotates at the center of the map
         robot_angle_rad = i / 180 * 2 * np.pi
-        robot_x_mm = true_map.width_mm * (0.4 + 0.2 * np.sin(robot_angle_rad*0.7))
+        robot_x_mm = true_map.width_mm * \
+            (0.4 + 0.2 * np.sin(robot_angle_rad*0.7))
         robot_y_mm = true_map.height_mm / 2
 
-        start_x_mm = robot_x_mm + tof_sensor_offset_mm * np.cos(robot_angle_rad)
-        start_y_mm = robot_y_mm + tof_sensor_offset_mm * np.sin(robot_angle_rad)
-        distance_mm = true_map.raycast((start_x_mm, start_y_mm), robot_angle_rad, tof_default_distance_mm)
+        start_x_mm = robot_x_mm + \
+            tof_sensor_offset_mm * np.cos(robot_angle_rad)
+        start_y_mm = robot_y_mm + \
+            tof_sensor_offset_mm * np.sin(robot_angle_rad)
+        distance_mm = true_map.raycast(
+            (start_x_mm, start_y_mm), robot_angle_rad, tof_default_distance_mm)
 
         if add_uncertainty:
             robot_angle_rad = np.random.normal(robot_angle_rad, np.radians(4))
@@ -46,32 +49,63 @@ def main():
     width_px = width_mm
     height_px = height_mm
 
-    true_map = envmap.Grid_map(width_mm, height_mm, width_px, height_px)
-    true_map.line_normalized((0.2, 0.3), (0.8, 0.1))
-    true_map.line_normalized((0.5, 0.7), (0.8, 0.7))
-    true_map.line_normalized((0.1, 0.1), (0.1, 0.9))
-    true_map.line_normalized((0.7, 0.3), (0.7, 0.7))
-    true_map.rectangle_normalized((0.38, 0.65), (0.4, 1))
-    cv2.imshow('True map', true_map.to_image())
+    true_map = envmap.Environment_map(width_mm, height_mm, cell_size_mm=1)
+    true_map.set_all_free()
+    true_map.occupied_line_normalized((0.2, 0.3), (0.8, 0.1))
+    true_map.occupied_line_normalized((0.5, 0.7), (0.8, 0.7))
+    true_map.occupied_line_normalized((0.1, 0.1), (0.1, 0.9))
+    true_map.occupied_line_normalized((0.7, 0.3), (0.7, 0.7))
+    true_map.occupied_rectangle_normalized((0.38, 0.65), (0.4, 1))
+    cv2.imshow('True map', true_map.to_image(width=width_px, height=height_px))
 
-    constructed_map = envmap.Constructed_map(
+    constructed_map = envmap.Environment_map(
         width_mm=width_mm, height_mm=height_mm, cell_size_mm=cell_size_mm)
 
-    data_generator = robot_data_generator(true_map, tof_sensor_offset_mm, tof_default_distance_mm, True)
+    data_generator = robot_data_generator(
+        true_map, tof_sensor_offset_mm, tof_default_distance_mm, True)
+
+    old_map_image = constructed_map.to_image(height=height_px, width=width_px)
 
     while(True):
 
-        robot_x_mm, robot_y_mm, robot_angle_rad, distance_mm = next(data_generator)
+        robot_x_mm, robot_y_mm, robot_angle_rad, distance_mm = next(
+            data_generator)
+
+        old_free = constructed_map.free_samples.copy()
+        old_total = constructed_map.total_samples.copy()
+        old_cells = constructed_map.cells.copy()
 
         constructed_map.construct((robot_x_mm, robot_y_mm), robot_angle_rad,
                                   robot_radius_mm, distance_mm, tof_sensor_offset_mm, tof_max_distance_mm)
 
-        img = constructed_map.to_image(height=height_px, width=width_px)
-        draw_robot(img, robot_x_mm, robot_y_mm, robot_angle_rad, robot_radius_mm, 0)
-        cv2.imshow('Constructed map', img)
-        cv2.imshow('Walkable', constructed_map.walkable_resized(robot_radius_mm, height_px, width_px))
+        map_image = constructed_map.to_image(height=height_px, width=width_px)
 
-        constructed_map.find_path((robot_x_mm, robot_y_mm), (100, 200), robot_radius_mm)
+        delta_image = cv2.subtract(map_image, old_map_image)
+        delta_image = cv2.resize(delta_image, (width_px//cell_size_mm,
+                                 height_px//cell_size_mm), interpolation=cv2.INTER_NEAREST)
+        old_map_image = map_image
+        if delta_image.max() > 0.5 or delta_image.min() < -0.5:
+            assert False
+
+        img = cv2.merge([map_image] * 3)
+
+        draw_robot(img, robot_x_mm, robot_y_mm, robot_angle_rad,
+                   robot_radius_mm, (0, 255, 0))
+
+        goal_mm = (width_mm//4, height_mm*5//6)
+        cv2.rectangle(img, goal_mm, np.add(
+            goal_mm, (cell_size_mm, cell_size_mm)), (255, 0, 0))
+        path = constructed_map.find_path(
+            (robot_x_mm, robot_y_mm), goal_mm, robot_radius_mm)
+        if len(path) >= 2:
+            for i in range(len(path)-1):
+                start = (int(path[i][0]), int(path[i][1]))
+                end = (int(path[i+1][0]), int(path[i+1][1]))
+                cv2.line(img, start, end, (0, 0, 255))
+
+        cv2.imshow('Constructed map', img)
+        cv2.imshow('Walkable', constructed_map.walkable_resized(
+            robot_radius_mm, height_px, width_px))
 
         k = cv2.waitKey(1)
         if k == 27:  # Esc
